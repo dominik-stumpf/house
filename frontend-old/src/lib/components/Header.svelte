2ac460115995f50968f461356b1cde79aafc367c
Dominik Stumpf <s.dominik.work@gmail.com>
2026-03-04 16:15:53 +0100

    chore: move /frontend

---
+++ frontend-old/src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { resolve } from "$app/paths";
+</script>
+
+<header>
+  <nav class="flex gap-4">
+    <a href={resolve("/")}>zgzg.dev</a>
+    <a href={resolve("/pof")}>proof of life</a>
+  </nav>
+</header>
