6eb96a993eaef5f79c02821a0f965ecdd305f8f9
Dominik Stumpf <s.dominik.work@gmail.com>
2026-03-16 19:20:52 +0100

    chore: remove experiments, update readme

--- README.md
+++ README.md
@@ -2,12 +2,4 @@
 
 ## Setup
 
-1. Provide secret env variables
-
-TUNNEL_TOKEN
-AUTH_TOKEN
-
-2. Create containers by providig the env file
-
-docker compose --env-file ~/.env up --build -d
-
+docker compose up
--- frontend-old/src/lib/components/Prose.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  let { children } = $props();
-</script>
-
-<div class="mx-8">
-  <div class="mx-auto my-16 prose prose-neutral md:my-24 lg:my-28 lg:prose-lg dark:prose-invert">
-    {@render children()}
-  </div>
-</div>
